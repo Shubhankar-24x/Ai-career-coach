bcf9b79b500cc2f2fc97152b48bb9ff789c624bc
Deepti Manna <www.deeptimanna786@gmail.com>
2025-03-05 03:46:39 +0530

    prepage interview page

--- app/(main)/interview/page.jsx
+++ app/(main)/interview/page.jsx
@@ -1,9 +1,24 @@
-const InterviewPage = () => {
-    return <div>
-        InterviewPage
+import { getAssessments } from "@/actions/interview";
+import PerformanceChart from "./components/performance-chart";
+import QuizList from "./components/quiz-list";
+import StatsCards from "./components/stats-cards";
+
+
+export default async function InterviewPrepPage() {
+  const assessments = await getAssessments();
+
+  return (
+    <div>
+      <div className="flex items-center justify-between mb-5">
+        <h1 className="text-6xl font-bold gradient-title">
+          Interview Preparation
+        </h1>
       </div>
-    
-  }
-  
-  export default InterviewPage
-  
+      <div className="space-y-6">
+        <StatsCards assessments={assessments} />
+        <PerformanceChart assessments={assessments} />
+        <QuizList assessments={assessments} />
+      </div>
+    </div>
+  );
+}
