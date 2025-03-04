b5ed386985b40b292c57a2a7b01edb907192c75b
Deepti Manna <www.deeptimanna786@gmail.com>
2025-03-05 02:42:58 +0530

    modified pages

--- app/(main)/dashboard/page.jsx
+++ app/(main)/dashboard/page.jsx
@@ -1,18 +1,22 @@
+import { getIndustryInsights } from "@/actions/dashboard";
+import DashboardView from "./_components/dashboard-view";
 import { getUserOnboardingStatus } from "@/actions/user";
 import { redirect } from "next/navigation";
 
-
-const IndustryInsightsPage = async() => {
+export default async function DashboardPage() {
   const { isOnboarded } = await getUserOnboardingStatus();
 
+  // If not onboarded, redirect to onboarding page
+  // Skip this check if already on the onboarding page
   if (!isOnboarded) {
     redirect("/onboarding");
   }
 
- 
-  return  <div>
-      IndustryInsightsPage
-  </div>
-}
+  const insights = await getIndustryInsights();
 
-export default IndustryInsightsPage
+  return (
+    <div className="container mx-auto">
+      <DashboardView insights={insights} />
+    </div>
+  );
+}
