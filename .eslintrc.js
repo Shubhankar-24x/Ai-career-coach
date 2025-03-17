1d4b520cb7d7b413583debcd7656f4abd865bef8
Shubhankar Biswas <shubhankar2821@gmail.com>
2025-03-17 11:18:04 +0530

    Update .eslintrc.js

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,5 +1,8 @@
-/** @type {import('eslint').Linter.Config} */
 module.exports = {
   root: true,
   extends: ["next/core-web-vitals"],
+  rules: {
+    "react-hooks/exhaustive-deps": "off",
+    "react/no-unescaped-entities": "off",
+  },
 };
