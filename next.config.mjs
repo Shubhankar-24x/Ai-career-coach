2157328eea3d9032c18b8de02cebd4498fcd0e8e
Shubhankar Biswas <shubhankar2821@gmail.com>
2025-03-29 21:55:56 +0530

    Update and rename next.config.js to next.config.mjs

---
+++ next.config.mjs
@@ -0,0 +1,15 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  // distDir: ".next", // Ensures build output is in `.next`
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "randomuser.me",
+      },
+    ],
+  },
+};
+
+export default nextConfig; // Keep using ES module export
+
