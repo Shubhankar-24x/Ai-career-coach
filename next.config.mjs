428eb233f142538c3b88897a45b561648b57a968
Shubhankar Biswas <shubhankar2821@gmail.com>
2025-04-04 12:56:49 +0530

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,24 +1,6 @@
-// /** @type {import('next').NextConfig} */
-// const nextConfig = {
-//   // distDir: ".next", // Ensures build output is in `.next`
-//   images: {
-//     remotePatterns: [
-//       {
-//         protocol: "https",
-//         hostname: "randomuser.me",
-//       },
-//     ],
-//   },
-// };
-
-// export default nextConfig; // Keep using ES module export
-
 /** @type {import('next').NextConfig} */
-import path from "path";
-
 const nextConfig = {
-  reactStrictMode: true, // Enables React strict mode for better debugging
-
+  // distDir: ".next", // Ensures build output is in `.next`
   images: {
     remotePatterns: [
       {
@@ -27,14 +9,8 @@ const nextConfig = {
       },
     ],
   },
-
-  webpack: (config) => {
-    config.resolve.alias = {
-      ...config.resolve.alias,
-      "@": path.resolve(__dirname), // Alias '@' to the root directory
-    };
-    return config;
-  },
 };
 
-export default nextConfig;
+export default nextConfig; // Keep using ES module export
+
+
