a5c17bfc222ecfb137eafe76ef2c118084d55234
Shubhankar Biswas <shubhankar2821@gmail.com>
2025-04-11 03:12:33 +0530

    updated file

--- app/(main)/resume/page.jsx
+++ app/(main)/resume/page.jsx
@@ -1,12 +1,5 @@
-// app/(main)/resume/page.jsx
-
-import dynamic from "next/dynamic";
 import { getResume } from "@/actions/resume";
-
-// Load ResumeBuilder on the client side only
-const ResumeBuilder = dynamic(() => import("./_components/resume-builder"), {
-  ssr: false,
-});
+import ResumeBuilder from "./_components/resume-builder";
 
 export default async function ResumePage() {
   const resume = await getResume();
